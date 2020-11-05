a4841f53cd552cdc128c851f05166c31b2078e1d
Spencer Gray <WhenWeedlesFly@gmail.com>
2020-11-04 20:24:22 -0500

    Notifications (fixed)

---
+++ PaySteps/PaySteps/Notifications.swift
@@ -0,0 +1,18 @@
+//
+//  Notifications.swift
+//  PaySteps
+//
+//  Created by Ryan Schuller on 11/4/20.
+//
+
+import Foundation
+import UserNotifications
+
+func sendNotification(title: String, subtitle: String = "", timeUntil: Double = 5.0,
+                      sound: UNNotificationSound = .default) {
+    let content = UNMutableNotificationContent()
+    (content.title, content.subtitle, content.sound) = (title, subtitle, sound)
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeUntil, repeats: false)
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+    UNUserNotificationCenter.current().add(request)
+}
